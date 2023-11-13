b93407911d1b35f2b129e986f283d0e4a16a1125
Naveen Perera <naveenperera9@gmail.com>
2023-11-13 22:25:54 +0530

    logic

--- src/screens/FinalScore.js
+++ src/screens/FinalScore.js
@@ -6,21 +6,28 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import Lottie from 'lottie-react-native';
 
 const FinalScore = ({ navigation }) => {
-  const [available, isAvailable] = useState(false);
+  const [available, isAvailable] = useState(true);
+  const [CbAvailable, isCbAvailable] = useState(false);
+  const [NsAvailable, isNsAvailable] = useState(false);
+
   const [name, setName] = useState('');
   const [age, setAge] = useState('');
+
   const [cbl1, setCbl1] = useState('');
   const [cbl2, setCbl2] = useState('');
   const [cbl3, setCbl3] = useState('');
+
   const [nl1, setNl1] = useState('');
   const [nl2, setNl2] = useState('');
 
   const [sumOfCB, setSumOfCB] = useState(0);
   const [sumOfCBTime, setSumOfCBTime] = useState(0);
+
   const [sumOfNS, setSumOfNS] = useState(0);
   const [sumOfNSTime, setSumOfNSTime] = useState(0);
 
-  const [threatLevel, setThreatLevel] = useState('');
+  const [cbThreatLevel, setCbThreatLevel] = useState('');
+  const [nsThreatLevel, setNsThreatLevel] = useState('');
 
   const [anim, setAnim] = useState(false);
 
@@ -44,33 +51,84 @@ const FinalScore = ({ navigation }) => {
 
         const n2 = await AsyncStorage.getItem("L2N");
         const n2Time = await AsyncStorage.getItem("L2NTime");
-        
-        if (nm !== null || ag !== null || cb1 !== null || cb2 !== null || n1 !== null || n2 !== null || cb3 !== null) {
+
+        if ((cb1 !== null && cb2 !== null && cb3 !== null) || (n1 !== null && n2 !== null)) {
           setName(nm);
           setAge(ag);
           setAnim(true);
 
-          setCbl1(`Color Blindness Level 01 Results ${cb1} / 5 ( spent ${cb1Time} seconds )`);
-          setCbl2(`Color Blindness Level 02 Results ${cb2} / 5 ( spent ${cb2Time} seconds )`);
-          setCbl3(`Color Blindness Level 03 Results ${cb3} / 5 ( spent ${cb3Time} seconds )`);
-          setNl1(`Nearsightedness Level 01 Results ${n1} / 6 ( spent ${n1Time} seconds )`);
-          setNl2(`Nearsightedness Level 02 Results ${n2} / 5 ( spent ${n2Time} seconds )`);     
+          if (cb1 !== null) {
+            isCbAvailable(true);
 
-          setSumOfCB( parseInt(cb1) + parseInt(cb2) + parseInt(cb3));
-          setSumOfCBTime( parseInt(cb1Time) + parseInt(cb2Time) + parseInt(cb3Time));
+            setCbl1(`Color Blindness Level 01 Results ${cb1} / 5 ( spent ${cb1Time} seconds )`);
+            setCbl2(`Color Blindness Level 02 Results ${cb2} / 5 ( spent ${cb2Time} seconds )`);
+            setCbl3(`Color Blindness Level 03 Results ${cb3} / 5 ( spent ${cb3Time} seconds )`);
 
-          setSumOfNS(parseInt(n1) + parseInt(n2));
-          setSumOfNSTime(parseInt(n1Time) + parseInt(n2Time));
+            setSumOfCB(parseInt(cb1) + parseInt(cb2) + parseInt(cb3));
+            setSumOfCBTime(parseInt(cb1Time) + parseInt(cb2Time) + parseInt(cb3Time));
 
-          if ( sumOfCB >= 11 && sumOfCBTime < 360 ){
-            setThreatLevel('Vision is very good');
+            if (sumOfCB >= 11 && sumOfCBTime < 360) {
+              setCbThreatLevel('Vision is very good');
+            }
+            else if (sumOfCB >= 8 && sumOfCB <= 10 && sumOfCBTime < 360) {
+              setCbThreatLevel('Vision has low threat');
+            }
+            else if (sumOfCB >= 4 && sumOfCB <= 7 && sumOfCBTime < 360) {
+              setCbThreatLevel('Vision has medium threat');
+            }
+            else if (sumOfCB < 4 && sumOfCBTime < 360) {
+              setCbThreatLevel('Vision has high threat');
+            }
+            else if (sumOfCB >= 11 && sumOfCBTime == 360) {
+              setCbThreatLevel('Vision has low threat');
+            }
+            else if (sumOfCB >= 8 && sumOfCB <= 10 && sumOfCBTime == 360) {
+              setCbThreatLevel('Vision has medium threat');
+            }
+            else if (sumOfCB >= 4 && sumOfCB <= 7 && sumOfCBTime == 360) {
+              setCbThreatLevel('Vision has high threat');
+            }
+            else if (sumOfCB < 4 && sumOfCBTime == 360) {
+              setCbThreatLevel('Vision has very high threat');
+            }
           }
-          else if ( sumOfCB >= 8 && sumOfCB <= 10 && sumOfCBTime < 360 ){
-            setThreatLevel('Vision has low threat level');
-          }
 
+          if (n1 !== null) {
+            isNsAvailable(true);
+
+            setNl1(`Nearsightedness Level 01 Results ${n1} / 6 ( spent ${n1Time} seconds )`);
+            setNl2(`Nearsightedness Level 02 Results ${n2} / 5 ( spent ${n2Time} seconds )`);
+
+            setSumOfNS(parseInt(n1) + parseInt(n2));
+            setSumOfNSTime(parseInt(n1Time) + parseInt(n2Time));
+
+            if (sumOfNS >= 8 && sumOfNSTime < 240) {
+              setNsThreatLevel('Vision is very good');
+            }
+            else if (sumOfNS >= 5 && sumOfNS <= 7 && sumOfNSTime < 240) {
+              setNsThreatLevel('Vision has low threat');
+            }
+            else if (sumOfNS >= 3 && sumOfNS <= 5 && sumOfNSTime < 240) {
+              setNsThreatLevel('Vision has medium threat');
+            }
+            else if (sumOfNS < 3 && sumOfNSTime < 240) {
+              setNsThreatLevel('Vision has high threat');
+            }
+            else if (sumOfNS >= 8 && sumOfNSTime == 240) {
+              setNsThreatLevel('Vision has low threat');
+            }
+            else if (sumOfNS >= 5 && sumOfNS <= 7 && sumOfNSTime == 240) {
+              setNsThreatLevel('Vision has medium threat');
+            }
+            else if (sumOfNS >= 3 && sumOfNS <= 5 && sumOfNSTime == 240) {
+              setNsThreatLevel('Vision has high threat');
+            }
+            else if (sumOfNS < 3 && sumOfNSTime == 240) {
+              setNsThreatLevel('Vision has very high threat');
+            }
+          }
         } else {
-          isAvailable(true);
+          isAvailable(false);
           console.log("No saved state");
         }
       } catch (error) {
@@ -92,9 +150,9 @@ const FinalScore = ({ navigation }) => {
               loop />
           </View>
         </> : <></>}
-      {available ?
+      {!available ?
         <>
-          <Text style={styles.Txt}>No Prevoius Saved Data</Text>
+          <Text style={styles.threatTxt}>Your Child Must Complete One Game Before Get the Results.</Text>
           <View style={styles.touchContainer}>
             <TouchableOpacity style={styles.tch} onPress={() => navigation.navigate('First')}>
               <Text style={styles.buttonText}>Start Playing</Text>
@@ -107,12 +165,25 @@ const FinalScore = ({ navigation }) => {
             <Text style={styles.Txt}>Name - {name}</Text>
             <Text style={styles.Txt}>Age - {age}</Text>
           </View>
-          <Text style={styles.threatTxt}>{threatLevel}</Text>
-          <Text style={styles.rTxt}>{cbl1}</Text>
-          <Text style={styles.rTxt}>{cbl2}</Text>
-          <Text style={styles.rTxt}>{cbl3}</Text>
-          <Text style={styles.rTxt}>{nl1}</Text>
-          <Text style={styles.rTxt}>{nl2}</Text>
+          {isCbAvailable && (
+            <>
+              <Text style={styles.rTxt}>Color Blindness Threat Level</Text>
+              <Text style={styles.threatTxt}>{cbThreatLevel}</Text>
+
+              <Text style={styles.rTxt}>{cbl1}</Text>
+              <Text style={styles.rTxt}>{cbl2}</Text>
+              <Text style={styles.rTxt}>{cbl3}</Text>
+            </>
+          )}
+          {isNsAvailable && (
+            <>
+              <Text style={styles.rTxt}>Nearsightedness Threat Level</Text>
+              <Text style={styles.threatTxt}>{nsThreatLevel}</Text>
+
+              <Text style={styles.rTxt}>{nl1}</Text>
+              <Text style={styles.rTxt}>{nl2}</Text>
+            </>
+          )}
           <View style={styles.touchContainer}>
             <TouchableOpacity style={styles.tch} onPress={() => navigation.navigate('First')}>
               <Text style={styles.buttonText}>Home</Text>
